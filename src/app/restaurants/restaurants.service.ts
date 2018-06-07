cb5ab5b88ecb651520d03a368193ba230533e018
Luan Henrique <lhppaixaoo@gmail.com>
2018-06-07 16:24:28 -0300

    Menu item component completo

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import {ErrorHandler} from '../app-handler';
+import {MenuItem} from '../restaurant-detail/menu-item/menu-item.model';
 
 
 @Injectable()
@@ -24,6 +25,11 @@ export class RestaurantsService {
     }
     reviewsOfRestaurant(id: string): Observable<any>{
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
+    }
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError);
     }
