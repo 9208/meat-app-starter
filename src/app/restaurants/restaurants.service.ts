dec9e7d1f0e96d397f87f0f10e00b3d10064f78f
Luan Henrique <lhppaixaoo@gmail.com>
2018-06-07 15:05:15 -0300

    Detalhes do restaurante, sistema de rotas

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -12,9 +12,13 @@ import {ErrorHandler} from '../app-handler';
 export class RestaurantsService {
     constructor(private http: Http) {
     }
-
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
+    }
+    restaurantsById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError);
     }
