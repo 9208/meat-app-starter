e0bb4ec0a1e71820272e317e0c2deca95843175b
Luan Paixão <lhppaixaoo@gmail.com>
2018-07-25 10:48:09 -0300

    Ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,64 +1,73 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID} from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, LOCALE_ID} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import {ROUTES} from './app.routes';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
 
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {RestaurantsComponent} from './restaurants/restaurants.component';
+import {RestaurantComponent} from './restaurants/restaurant/restaurant.component';
 import {RestaurantsService} from './restaurants/restaurants.service';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import {RestaurantDetailComponent} from './restaurant-detail/restaurant-detail.component';
+import {MenuComponent} from './restaurant-detail/menu/menu.component';
+import {ShoppingCartComponent} from './restaurant-detail/shopping-cart/shopping-cart.component';
+import {MenuItemComponent} from './restaurant-detail/menu-item/menu-item.component';
+import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component';
 import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
+import {OrderComponent} from './order/order.component';
 
+import {InputComponent} from './shared/input/input.component';
+import { RadioComponent} from './shared/radio/radio.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
+    declarations: [
+        AppComponent,
+        HeaderComponent,
 
-    HomeComponent,
+        HomeComponent,
 
-    AboutComponent,
+        AboutComponent,
 
-    RestaurantsComponent,
+        RestaurantsComponent,
 
-    RestaurantComponent,
+        RestaurantComponent,
 
-    RestaurantDetailComponent,
+        RestaurantDetailComponent,
 
-    MenuComponent,
+        MenuComponent,
 
-    ShoppingCartComponent,
+        ShoppingCartComponent,
 
-    MenuItemComponent,
+        MenuItemComponent,
 
-    ReviewsComponent,
+        ReviewsComponent,
 
-    OrderComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    RouterModule.forRoot(ROUTES),
+        OrderComponent,
 
-  ],
-  providers: [
-      RestaurantsService,
-      ShoppingCartService,
-      {provide: LOCALE_ID, useValue: 'pt-BR'}
-  ],
-  bootstrap: [AppComponent]
+        InputComponent,
+
+        RadioComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot(ROUTES),
+
+    ],
+    providers: [
+        RestaurantsService,
+        ShoppingCartService,
+        {provide: LOCALE_ID, useValue: 'pt-BR'}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
